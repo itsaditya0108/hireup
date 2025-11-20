4ad392d08dcfea7f928aab4bf51e5b2a2363d4d1
Aditya <adityaverma90055@gmail.com>
2025-11-20 08:50:55 +0530

    Added vercel file

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
 import { dark } from '@clerk/themes'
 
-// Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
