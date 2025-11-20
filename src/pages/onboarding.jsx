9269673a9af6a07e18c5bc821e90b71db3174f8f
Aditya <adityaverma90055@gmail.com>
2025-11-20 11:43:12 +0530

    Updated onboarding.jsx file

--- src/pages/onboarding.jsx
+++ src/pages/onboarding.jsx
@@ -1,50 +1,43 @@
-import { Button } from "@/components/ui/button";
 import { useUser } from "@clerk/clerk-react";
-import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import { BarLoader } from "react-spinners";
 
 const Onboarding = () => {
     const { user, isLoaded } = useUser();
     const navigate = useNavigate();
-    const [updatingRole, setUpdatingRole] = useState(false);
 
-    // Handle role selection
-    const handleRoleSelection = async (role) => {
-        try {
-            setUpdatingRole(true);
-            await user.update({ publicMetadata: { role } });
-
-            // Small delay to ensure Clerk syncs the session
-            setTimeout(() => {
-                navigate(role === "recruiter" ? "/post-job" : "/jobs");
-            }, 500);
-        } catch (err) {
-            console.log("Error updating role:", err);
-        } finally {
-            setUpdatingRole(false);
-        }
+    const navigateUser = (currRole) => {
+        navigate(currRole === "recruiter" ? "/post-job" : "/jobs");
     };
 
-    // Redirect only if role exists
-    useEffect(() => {
-        if (isLoaded && user?.publicMetadata?.role) {
-            const role = user.publicMetadata.role;
-            navigate(role === "recruiter" ? "/post-job" : "/jobs");
-        }
-    }, [isLoaded, user, navigate]);
+    const handleRoleSelection = async (role) => {
+        await user
+            .update({ unsafeMetadata: { role } })
+            .then(() => {
+                console.log(`Role updated to: ${role}`);
+                navigateUser(role);
+            })
+            .catch((err) => {
+                console.error("Error updating role:", err);
+            });
+    };
 
-    if (!isLoaded || updatingRole) {
+    useEffect(() => {
+        if (user?.unsafeMetadata?.role) {
+            navigateUser(user.unsafeMetadata.role);
+        }
+    }, [user]);
+
+    if (!isLoaded) {
         return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
     }
 
-    // If user has role, return null while redirecting
-    if (user?.publicMetadata?.role) return null;
-
     return (
-        <div className="flex flex-col items-center justify-center mt-32">
+        <div className="flex flex-col items-center justify-center mt-40">
             <h2 className="gradient-title font-extrabold text-7xl sm:text-8xl tracking-tighter">
-                I am a ..
+                I am a...
             </h2>
             <div className="mt-16 grid grid-cols-2 gap-4 w-full md:px-40">
                 <Button
