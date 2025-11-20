c4a117a232ffdce3128a977c86e77913496725c8
Aditya <adityaverma90055@gmail.com>
2025-11-20 11:24:47 +0530

    Updated main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, useNavigate } from "react-router-dom";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { shadesOfPurple } from "@clerk/themes";
 import App from "./App.jsx";
@@ -16,27 +16,15 @@ if (!FRONTEND_API) {
   throw new Error("Missing VITE_CLERK_FRONTEND_API");
 }
 
-function ClerkWithRouter({ children }) {
-  const navigate = useNavigate();
-  return (
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <ClerkProvider
       publishableKey={PUBLISHABLE_KEY}
       frontendApi={FRONTEND_API}
       appearance={{ baseTheme: shadesOfPurple }}
-      navigate={(to) => navigate(to)}
       afterSignOutUrl="/"
     >
-      {children}
+      <App />
     </ClerkProvider>
-  );
-}
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ClerkWithRouter>
-        <App />
-      </ClerkWithRouter>
-    </BrowserRouter>
   </React.StrictMode>
 );
